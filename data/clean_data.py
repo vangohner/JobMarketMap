4a98cf2ae0fdc1ce419c67aac8f41d9235706e59
Carter Hendrickson <carterthendrickson@gmail.com>
2025-09-27 11:06:18 -0400

    small edit for cleaning script

--- data/clean_data.py
+++ data/clean_data.py
@@ -3,29 +3,32 @@ import pandas as pd
 df = pd.read_csv("postings.csv")
 
 # remove the columns we don't want
-df = df.drop(
-    [
-        "compensation_type", 
-        "expiry", 
-        "applies", 
-        "application_url", 
-        "application_type", 
-        "closed_time", 
-        "work_type",
-        "views",
-        "posting_domain",
-        "pay_period",
-        "company_id",
-        "job_posting_url",
-        "sponsored",
-        "currency",
-        "max_salary",
-        "med_salary",
-        "min_salary",
-        "listed_time"
-    ], 
-    axis="columns"
-)
+try:
+    df = df.drop(
+        [
+            "compensation_type", 
+            "expiry", 
+            "applies", 
+            "application_url", 
+            "application_type", 
+            "closed_time", 
+            "work_type",
+            "views",
+            "posting_domain",
+            "pay_period",
+            "company_id",
+            "job_posting_url",
+            "sponsored",
+            "currency",
+            "max_salary",
+            "med_salary",
+            "min_salary",
+            "listed_time"
+        ], 
+        axis="columns"
+    )
+except KeyError:
+    print("columns already removed.")
 
 # drop rows with null values
 df = df.dropna(
